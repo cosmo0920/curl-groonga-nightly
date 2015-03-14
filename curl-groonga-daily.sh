09edee993d7193dc6072992a95cf2032d6406d01
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2015-03-14 16:48:34 +0900

    Add getting nightly groonga tar.gz package script

---
+++ curl-groonga-daily.sh
@@ -0,0 +1,18 @@
+# specify MariaDB and Mroonga version
+export GROONGAVER="5.0.0"
+# get Date
+# e.g. # => 2014.07.01
+export DATE="`date +\"%Y.%m.%d\"`"
+export keep_n_days=30
+#xarg option
+XARGS_NO_RUN=''
+if [ `uname` = 'Linux' ]; then
+    XARGS_NO_RUN='--no-run-if-empty';
+fi
+# remove older than 30 days nightly package
+find ./*.zip -maxdepth 1 -type f -ctime +${keep_n_days} | \
+  xargs $XARGS_NO_RUN rm
+# curl
+set -x
+curl --fail -O "http://packages.groonga.org/nightly/groonga-${GROONGAVER}.${DATE}.tar.gz"
+
