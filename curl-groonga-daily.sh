c778d5c35b023f478110041bd5835ba157644e70
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2015-03-15 14:04:58 +0900

    zip -> tar.gz

--- curl-groonga-daily.sh
+++ curl-groonga-daily.sh
@@ -10,7 +10,7 @@ if [ `uname` = 'Linux' ]; then
     XARGS_NO_RUN='--no-run-if-empty';
 fi
 # remove older than 30 days nightly package
-find ./*.zip -maxdepth 1 -type f -ctime +${keep_n_days} | \
+find ./*.tar.gz -maxdepth 1 -type f -ctime +${keep_n_days} | \
   xargs $XARGS_NO_RUN rm
 # curl
 curl --fail -O "http://packages.groonga.org/nightly/groonga-${GROONGAVER}.${DATE}.tar.gz"
